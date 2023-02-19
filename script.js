3da1573cac8b4291c77f9faa129b28acd4d3b6c2
Najaf Sharifov <nwerifov@mail.ru>
2023-02-19 21:41:37 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//Task 2.5
+
+/*let doubleDigits = prompt("Enter three double digit number in format aa,bb,cc")
+let nums = doubleDigits.split(",");
+
+
+if((nums[0]>nums[1]) && (nums[0]<nums[2]) || (nums[0]<nums[1] && nums[0]>nums[2])){
+    console.log(`"Orta ədəd ${nums[0]}-dir"`);
+} else if ((nums[1]>nums[0] && nums[1]<nums[2]) || (nums[1]<nums[0] && nums[1]>nums[2])){
+    console.log(`"Orta ədəd ${nums[1]}-dir"`);
+}else{
+    console.log(`"Orta ədəd ${nums[2]}-dir"`);
+}
+
+
+if((nums[0] == nums[1]) || (nums[0] == nums[2]) || (nums[1] == nums[2])) {
+    console.log("İki və ya üç daxil edilmiş ədəd eynidir");
+}
+
+
+if(isNaN(nums[0]) && isNaN(nums[1]) && isNaN(nums[2])) {
+    console.log("Yanliş dəyər daxil edilib");
+}*/
+
+//Task 2.5 Bonus
+//Ternary
+/*let year = +prompt("Enter random year");
+year % 4 == 0 && year % 100 == 0 && year % 400 != 0 ? console.log("İl uzun ildir") : console.log("İl uzun il deyil");
+
+//if...else
+if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0) {
+  console.log("İl uzun ildir");
+} else {
+  console.log("İl uzun il deyil");
+}*/
